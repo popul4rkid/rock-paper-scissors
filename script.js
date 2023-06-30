fbb54a81704a2223fc4dda13a0d114320e3603a7
popul4rkid <mattugh@gmail.com>
2023-06-29 20:49:13 -0400

    Establish rules for the game and test functionality

--- script.js
+++ script.js
@@ -4,11 +4,43 @@
 
 function getComputerChoice(){
     const rps = ["rock" , "paper" , "scissor"];
-    const choice = Math.floor(Math.random() * rps.length);
-    const response = rps[choice];
-    return response
+    const choice = Math.floor(Math.random() * 3);
+    return rps[choice]
 };
+// returns rock, paper, scissor at random
 
-console.log(getComputerChoice())
+function playRound(playerSelection, compSelection){
+    if (playerSelection === 'rock' && compSelection === 'rock'){
+        return 'Welp, you tied :/'
+    } else if (playerSelection === 'paper' && compSelection === 'paper'){
+        return 'Welp, you tied'
+    } else if (playerSelection === 'scissor' && compSelection === 'scissor'){
+        return 'Welp, you tied'
+    } else if (playerSelection === 'rock' && compSelection === 'scissor'){
+        return 'Rock destroys scissors, you\'ve won'
+    } else if (playerSelection === 'rock' && compSelection === 'paper' ){
+        return 'Paper beats rock, try again'
+    } else if (playerSelection === 'paper' && compSelection === 'scissor'){
+        return 'Scissor beats paper, try again'
+    } else if (playerSelection === 'paper' && compSelection === 'rock'){
+        return 'Paper beats rock, you win'
+    } else if (playerSelection === 'scissor' && compSelection === 'rock'){
+        return 'Rock destroys scissors, try again'
+    } else if (playerSelection === 'scissor' && compSelection === 'paper'){
+        return 'Scissors beats paper, you win'
+    }
+
+}
+
+// establishes the rules of the game between the player selection and computer selection
+
+function playround(playerSelection, compSelection){
+
+}
+
+const playerSelection = 'rock';
+const compSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, compSelection))
 
 
