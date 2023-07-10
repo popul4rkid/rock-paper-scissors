69195c5eb3adb38a67cb922742d55d2103090210
popul4rkid <mattugh@gmail.com>
2023-07-10 19:12:00 -0400

    Adjust game logic for score to increase incrementally

--- script.js
+++ script.js
@@ -7,55 +7,56 @@ function getComputerChoice(){
     const choice = Math.floor(Math.random() * 3);
     return rps[choice]
 };
-console.log(getComputerChoice)
+
 
 // returns rock, paper, scissor at random
 
 
+
 function playRound(playerSelection, compSelection){
-    if (playerSelection === 'rock' && compSelection === 'rock'){
-        return 'Welp, you tied :/'
-    } else if (playerSelection === 'paper' && compSelection === 'paper'){
+    if (playerSelection.toLowerCase() === 'rock' && compSelection === 'rock'){
         return 'Welp, you tied'
-    } else if (playerSelection === 'scissor' && compSelection === 'scissor'){
+    } else if (playerSelection.toLowerCase() === 'paper' && compSelection === 'paper'){
         return 'Welp, you tied'
-    } else if (playerSelection === 'rock' && compSelection === 'scissor'){
+    } else if (playerSelection.toLowerCase() === 'scissor' && compSelection === 'scissor'){
+        return 'Welp, you tied'
+    } else if (playerSelection.toLowerCase() === 'rock' && compSelection === 'scissor'){
         return 'Rock destroys scissors, you\'ve won'
-    } else if (playerSelection === 'rock' && compSelection === 'paper' ){
+    } else if (playerSelection.toLowerCase() === 'rock' && compSelection === 'paper' ){
         return 'Paper beats rock, try again'
-    } else if (playerSelection === 'paper' && compSelection === 'scissor'){
+    } else if (playerSelection.toLowerCase() === 'paper' && compSelection === 'scissor'){
         return 'Scissor beats paper, try again'
-    } else if (playerSelection === 'paper' && compSelection === 'rock'){
+    } else if (playerSelection.toLowerCase() === 'paper' && compSelection === 'rock'){
         return 'Paper beats rock, you win'
-    } else if (playerSelection === 'scissor' && compSelection === 'rock'){
+    } else if (playerSelection.toLowerCase() === 'scissor' && compSelection === 'rock'){
         return 'Rock destroys scissors, try again'
-    } else if (playerSelection === 'scissor' && compSelection === 'paper'){
+    } else if (playerSelection.toLowerCase() === 'scissor' && compSelection === 'paper'){
         return 'Scissors beats paper, you win'
-    }
-
-}
-
-let playerSelection = "rock";
-let compSelection = getComputerChoice;
-console.log(playRound(playerSelection, compSelection))
-
-// establishes the rules of the game between the player selection and computer selection
-
-function game() {
-    for (i = 0; i < 5; i++){
-        let playerSelection = prompt('rock, paper, scissors', 'rock')
-        let compSelection = getComputerChoice;
-        playRound(playerSelection, compSelection);
-        console.log(playRound(playerSelection,compSelection));
     }
     
 }
 
 
 
+// establishes the rules of the game between the player selection and computer selection
+
+function game() {
+    for (i = 0; i < 5; i++){
+        let playerSelection = prompt('rock, paper, scissors')
+        let compSelection = getComputerChoice();
+        playRound(playerSelection, compSelection);
+        console.log(playRound(playerSelection, compSelection))
+    }
+    
+}
 // game is initialized; player input is demanded via prompt and computer selection is demanded via previous called function. 
 // this is looped 5 times
 
+if ('Welp, you tied' === 3) {
+    alert('Welp, you tied')
+}
+
+console.log(game())
 
 
 
